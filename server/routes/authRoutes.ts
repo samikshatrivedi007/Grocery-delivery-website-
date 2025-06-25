5c1d902a09feafeea612211d51366d143ba554d6
Samiksha <samikshatrivedi007@gmail.com>
2025-06-25 23:09:21 +0530

    authRoutes✅

--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -1,32 +1,9 @@
-import mongoose, {Document, Schema} from 'mongoose';
-export interface IUser extends Document {
-    name:string;
-    email:string;
-    phone?:string;
-    password:string;
-    role:string;
-}
-const userSchema :Schema =new Schema<IUser>({
-        name:{
-            type:String,
-            required:true,
-        },
-        email:{
-            type:String,
-            required :true,
-            unique:true,
-        },
-        phone:{
-            type:String,
-        },
-        password:{
-            type:String,
-            required:true,
-        },
-        role:{
-            type:String,
-            default:'user',
-        },
-    },
-    { timestamps: true });
-export default mongoose.model<IUser>('User', userSchema);
+import { Router } from 'express';
+import { register, login } from '../controllers/authController';
+
+const router = Router();
+
+router.post('/register', register);
+router.post('/login', login);
+
+export default router;
